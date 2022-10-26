de529d79ec0f19b0c7c6c019cb8aa5bfe9dfa80b
Sepehr Nematollahi <sepehr.nematollahi@snapp.cab>
2022-10-26 05:32:40 +0330

    done

--- main.go
+++ main.go
@@ -17,11 +17,16 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
+	counter := 1
 	for scanner.Scan() {
 		company := scanner.Text()
 		fmt.Println(company)
-		exec.Command("open", company).Start()
-		fmt.Scanln()
+		exec.Command("open", "https://www.google.com/search?q="+company).Start()
+		if counter == 10 {
+			fmt.Scanln()
+			counter = 0
+		}
+		counter++
 	}
 
 	if err := scanner.Err(); err != nil {
